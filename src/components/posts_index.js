6df99b8e4d9e13b82d6b75af6612d94e94680a79
KEITH WALKER <keith@KEITHs-MBP.fios-router.home>
2017-01-31 09:10:31 -0500

    Updating status

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions/index';
 
-export default () => {
-  return (
-    <div>List of blog posts.</div>
-  )
+class PostsIndex extends Component {
+  componentWillMount() {
+    this.props.fetchPosts();
+  }
+
+  render() {
+    return (
+      <div>List of blog posts</div>
+    );
+  }
 }
+
+// function mapDispatchToProps(dispatch) {
+//   return bindActionCreators({ fetchPosts }, dispatch);
+// }
+// {fetchPosts: fetchPosts} below shortcuts this mapDispatchToProps above.
+
+export default connect(null, { fetchPosts })(PostsIndex);
